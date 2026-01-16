a00f5d1520fc95a41b41bd383460ce4321f0880f
dnl07 <daniel.baranovskiy07@gmail.com>
2026-01-16 11:09:18 +0100

    Improve fuzzy search performance by adding bounds

--- Apollon.Core/Fuzzy/FuzzyMatcher.cs
+++ Apollon.Core/Fuzzy/FuzzyMatcher.cs
@@ -14,20 +14,39 @@ namespace Apollon.Core.Fuzzy {
         }
 
         public IReadOnlyList<FuzzyToken> Match(string token, TokenRegistry tokenRegistry, QueryOptions queryOptions) {
-            var candidates = new HashSet<int>();
+            var candidates = new Dictionary<string, int>();
 
             foreach (var nGram in NGramGenerator.Generate(token, _indexOptions.NGramSize)) {
-                candidates = candidates.Concat(_nGramIndex.GetCandidates(nGram)).ToHashSet();
+                foreach (var candidateId in _nGramIndex.GetCandidates(nGram)) {
+                    var candidate = tokenRegistry.GetToken(candidateId);
+
+                    if (candidates.TryGetValue(candidate, out var count)) {
+                        candidates[candidate] = count + 1;
+                    } else {
+                        candidates[candidate] = 1;
+                    }
+                }
             }
 
             List<FuzzyToken> tokens = new List<FuzzyToken>();
 
-            foreach (var id in candidates) {
-                string candidate = tokenRegistry.GetToken(id);
 
-                var editDistance = EditDistance.Calculate(token, candidate);
+            var cand = new List<string>();
+            foreach ((string key, int c) in candidates) {
+                var nGramsA = NGramGenerator.Generate(token, _indexOptions.NGramSize);
+                var nGramsB = NGramGenerator.Generate(key, _indexOptions.NGramSize);
+
+                if (c >= Math.Max(nGramsA.Count, nGramsB.Count) - _indexOptions.NGramSize * queryOptions.MaxEditDistance)
+                        {
+                            cand.Add(key);
+                        }
+            }
+
+
+            foreach (var id in cand) {
+                var editDistance = EditDistance.Calculate(token, id);
                 if (editDistance <= queryOptions.MaxEditDistance) {
-                    tokens.Add(new FuzzyToken(candidate, editDistance));
+                    tokens.Add(new FuzzyToken(id, editDistance));
                 }    
             }
 
